a381de51c0774a558a604d64517338430d6b89f8
philoinovsky <1129410550@qq.com>
2020-09-09 09:29:19 +0000

    update

--- interface/urls.py
+++ interface/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('contact', views.contact, name='contact'),
+    path('aboutUs', views.aboutUs, name='aboutUs'),
+    path('details', views.details, name='details'),
+    path('help', views.help, name='help'),
+    path('EEE', views.EEE, name='EEE'),
+    path('awards', views.awards, name='awards'),
 ]
--- interface/views.py
+++ interface/views.py
@@ -11,3 +11,28 @@ def contact(request):
     template = loader.get_template('interface/contact.html')
     context = {}
     return HttpResponse(template.render(context, request))
+
+def aboutUs(request):
+    template = loader.get_template('interface/aboutUs.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def details(request):
+    template = loader.get_template('interface/details.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def help(request):
+    template = loader.get_template('interface/help.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def EEE(request):
+    template = loader.get_template('interface/EEE.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def awards(request):
+    template = loader.get_template('interface/awards.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
